e0124d074136b619e2302d3d22200a20ad7ac1b3
Eric Galloix <egalloix1@gmail.com>
2018-08-26 19:39:39 +0300

    master: added new debug prints, use of dirname and stats for check permissions of a file. Also added hook for the OS call open to see what happens with a java app

--- preloadWrite.c
+++ preloadWrite.c
@@ -7,7 +7,10 @@
 #include <stdio.h>
 #include <limits.h>
 
+#include <libgen.h>
+
 ssize_t (*orig_write)(int , const void *, size_t ) = NULL;
+int (*orig_open)(char* , int, mode_t ) = NULL;
 
 void printDebug(char* printThis)
 {
@@ -21,26 +24,76 @@ extern "C" ssize_t write(int fd, const void *buf, size_t count)
 
 	char wrbuf[PATH_MAX];
 	char lnkbuf[PATH_MAX];
+	memset(wrbuf,0,PATH_MAX);
+	memset(lnkbuf,0,PATH_MAX);
     snprintf(wrbuf,PATH_MAX,"/proc/%d/fd/%d",getpid(),fd);
 	readlink(wrbuf, lnkbuf, PATH_MAX);
 	int temp=strlen(lnkbuf);
 	lnkbuf[temp]='\n';
-	lnkbuf[temp+1]=0;
 	
 	char blackListedStr[]="hsperfdata";
 	if (NULL==strstr(lnkbuf, blackListedStr))
 	{
+		wrbuf[0]='\n';
+		wrbuf[1]='L';
+		wrbuf[2]='N';
+		wrbuf[3]='W';
+		wrbuf[4]='\n';
+		wrbuf[5]=0;
+		printDebug(wrbuf);
 		printDebug(lnkbuf);
 		return (*orig_write)(fd, buf, count);
 	}
 	else
-	{
+	{	
 		return 0;	
 	}
 	
 
 }
 
+extern "C" int open(char* fname, int flags, mode_t aMode)
+{
+	// !! Seems that a java app won't call this function on file open
+	// inside the app.
+	// On the other hand, java.exe seems to call open, e.g to open the jar file
+	// to be executed.
+	
+	if ( orig_write == NULL )
+        orig_write = (ssize_t (*)(int , const void *, size_t))dlsym(RTLD_NEXT, "write");
+	if ( orig_open == NULL )
+        orig_open = (ssize_t (*)(char* , int, mode_t))dlsym(RTLD_NEXT, "open");
+
+	// test on RO dir path of a file  (use dirname)
+    char wrbuf[PATH_MAX];
+	memset(wrbuf,0,PATH_MAX);
+	struct stat fileStat;
+	char fnameTest[] = "/proc/sys/fs/file-nr";
+	char* ts1 = strdup(fnameTest); // str duplicate, no need to free mem!
+	stat( dirname(ts1), &fileStat); // dirname in #include <libgen.h>
+
+	if ( (fileStat.st_mode & S_IWUSR) == 0 &&  (fileStat.st_mode & S_IRUSR) )
+	{
+		wrbuf[0]='R';
+		wrbuf[1]='O';
+		wrbuf[2]='\n';
+		printDebug(wrbuf);
+	}
+
+	wrbuf[0]='\n';
+	wrbuf[1]='-';
+	wrbuf[2]='-';
+	wrbuf[3]='\n';	
+	printDebug(wrbuf);
+	printDebug(fname);
+	wrbuf[0]='\n';
+	wrbuf[1]='+';
+	wrbuf[2]='+';
+	wrbuf[3]='\n';	
+    printDebug(wrbuf);
+	   
+	return (*orig_open)(fname, flags, aMode);
+}
 
 
 
